d86aa4aa383d0eb913fd9f432355b87d3cc3c459
zhoumeiyan <zhoumeiyan@wecash.net>
2018-01-16 15:39:13 +0800

    update .gitignore

--- util.js
+++ util.js
@@ -1,6 +1,12 @@
 /**
  * Created by zhoumeiyan on 2018/1/16.
  */
+let store = {
+    storage: window.localStorage,
+    session: {
+        storage: window.sessionStorage
+    }
+}
 export function getElementLeft(el) {
     let actualLeft = el.offsetLeft
     let current = el.offsetParent
@@ -21,4 +27,83 @@ export function getElementTop(el) {
     return actualTop
 }
 
+export function hasClass(el, className) {
+    let reg = new RegExp(`(^|\\s)${className}($|\\s)`)
+    return reg.test(el.className)
+}
 
+export function addClass(el, className) {
+    if (hasClass(el, className)) {
+        return
+    }
+    let newClass = el.split(',')
+    newClass.push(className)
+    el.className = newClass.join(' ')
+}
+
+export function getData(el, name, val) {
+    const prefix = 'data-'
+    if (val) {
+        return el.setAttribute(`${prefix}${name}`, val)
+    }
+    return el.getAttribute(`${prefix}${name}`)
+}
+
+let elementStyle = document.createElement('div').style
+let vendor = (() => {
+    let transformNames = {
+        webkit: 'webkitTransform',
+        Moz: 'MozTransform',
+        O: 'OTransform',
+        ms: 'msTransform',
+        standard: 'transform'
+    }
+    for (let key in transformNames) {
+        if (elementStyle[transformNames[key]] !== undefined) {
+            return key
+        }
+    }
+    return false
+})()
+
+export function prefixStyle(style) {
+    if (!vendor) {
+        return false
+    }
+    if (vendor === 'standard') {
+        return style
+    }
+    return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
+
+export function getquerystring (name, url) {
+    let reg = new RegExp(`(^|&)${name}=([^&]*)($|&)`)
+    let r = url.match(reg)
+    if (!r) {
+        return decodeURIComponent(r[2])
+    }
+}
+
+export function setStorage(key, val) {
+    if (!val) {
+        return
+    }
+    if (val && (typeof val === 'object')) {
+        return store.storage.setItem(key, JSON.stringify(val))
+    }
+    return store.storage.setItem(key, val)
+}
+
+export function getStorage(key) {
+    let val = store.storage.getItem(key)
+    if (val === undefined) {
+        return
+    }
+    return JSON.parse(val)
+}
+
+export function removeStorage(key) {
+    return Array.isArray(key) ? key.forEach((item) => {
+        store.storage.removeItem(item)
+    }) : store.storage.removeItem(key)
+}
