8fcd1ea3e1ca0337da05b3c8f0b4411677b139ee
zhoumeiyan <312793179@qq.com>
2019-01-29 11:36:27 +0800

    [update]修改公用的dom操作方法

--- js/util.js
+++ js/util.js
@@ -28,17 +28,14 @@ export function getElementTop(el) {
 }
 
 export function hasClass(el, className) {
-    let reg = new RegExp(`(^|\\s)${className}($|\\s)`)
-    return reg.test(el.className)
+  return el.classList.contains(className)
 }
 
 export function addClass(el, className) {
-    if (hasClass(el, className)) {
-        return
-    }
-    let newClass = el.split(',')
-    newClass.push(className)
-    el.className = newClass.join(' ')
+  if (hasClass(el, className)) {
+    return
+  }
+  el.classList.add(className)
 }
 
 export function getData(el, name, val) {
